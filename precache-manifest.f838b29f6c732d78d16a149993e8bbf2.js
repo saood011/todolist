13b19397d990b34728b247fdeb881d4617978283
root <saood011@yahoo.com>
2020-01-28 13:55:19 +0100

    Updates

---
+++ precache-manifest.f838b29f6c732d78d16a149993e8bbf2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbdb35e56382a4a9623c926955f9cc1f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "3a64ef54ffef61807687",
+    "url": "/todolist/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "49c21d7b0dac01fd7805",
+    "url": "/todolist/static/css/main.f8e3344a.chunk.css"
+  },
+  {
+    "revision": "3a64ef54ffef61807687",
+    "url": "/todolist/static/js/2.d2b49e4f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist/static/js/2.d2b49e4f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49c21d7b0dac01fd7805",
+    "url": "/todolist/static/js/main.67cd0045.chunk.js"
+  },
+  {
+    "revision": "cebccde0f7f76a903cc9",
+    "url": "/todolist/static/js/runtime-main.6fa7f924.js"
+  },
+  {
+    "revision": "b16629e033d1376e19a9d75ac5cb5b0f",
+    "url": "/todolist/static/media/task.b16629e0.jpeg"
+  }
+]);
